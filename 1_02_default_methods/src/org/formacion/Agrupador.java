cb9a6793d4d0ff7dcd479a5ee036234dfdc8d213
franklin <franklin.cruces@hotmail.com>
2024-06-21 23:52:01 -0400

    primer commit

---
+++ 1_02_default_methods/src/org/formacion/Agrupador.java
@@ -0,0 +1,17 @@
+package org.formacion;
+
+import java.util.Collection;
+
+public interface Agrupador {
+
+	void add (Object elemento);
+	
+	int numeroElementos();
+
+	default void addAll(Collection col) {
+	    for(Object o: col) {
+	    	add(o);
+	    }
+	}
+	
+}
